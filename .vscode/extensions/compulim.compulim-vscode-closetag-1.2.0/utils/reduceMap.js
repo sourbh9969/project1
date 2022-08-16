5a73c70d92122f4d10860a9bac054ae9db8e3bfc
sourbh9969 <108663108+sourbh9969@users.noreply.github.com>
2022-08-16 10:57:35 +0530

    Delete .vscode/extensions directory

--- .vscode/extensions/compulim.compulim-vscode-closetag-1.2.0/utils/reduceMap.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function reduceMap(map, reducer, initial) {
-  return Object.keys(map).reduce((result, name) => {
-    return reducer.call(map, result, map[name], name);
-  }, initial);
-}
-
-module.exports = reduceMap;
